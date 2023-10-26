b0269b096cdfb4bc22692023aac3dc579d47d238
Stojj2 <>
2023-10-26 17:41:19 +0000

    Add content to Hypothesis_and_validation.py page for streamlit

--- app_pages/hypothesis_and_validation.py
+++ app_pages/hypothesis_and_validation.py
@@ -2,4 +2,25 @@ import streamlit as st
 
 
 def HypothesisAndValidation():
-    st.write("Testing the 'hypothesis_and_validation' page and it seems to work fine!")
+    st.header("Hypothesis And Validation")
+
+    st.info(
+        f"**Hypothesis (H1)**"
+        f"\n\nThe neural network can achieve an accuracy"
+        f"of at least 97% in distinguishing powdery mildew leaves from non-powdery mildew leaves in the dataset,"
+        f" demonstrating its effectiveness in this task."
+        )
+    
+
+    st.success(
+        f"**Model performance (H1)** "
+        F"\n\n The training of the model was conducted with various batch sizes, and it became evident"
+        f" that a batch size of 15 yielded the highest accuracy"
+        f" on the ** test_set**. Therefore, we can confidently conclude that the hypothesis(H1) has been validated based on this results"
+        f""
+        f"\n\n"
+        f"\n\n - bach_size of 10 - 2s 17ms/step - loss: 0.0276 - accuracy: 0.9953"
+        f"\n\n - **bach_size of 15 - 1s 24ms/step - loss: 0.0081 - accuracy: 0.9964 **"
+        f"\n\n - bach_size of 20 - 2s 28ms/step - loss: 0.0451 - accuracy: 0.9882"
+        f"\n\n - bach_size of 25 - 2s 40ms/step - loss: 0.0123 - accuracy: 0.9953"
+        )
--- app_pages/model_prediction_analysis.py
+++ app_pages/model_prediction_analysis.py
@@ -1,18 +1,5 @@
 import streamlit as st
-import streamlit as st
-from PIL import Image
-import numpy as np
-import pandas as pd
-
-from src.data_management import download_dataframe_as_csv
-from src.machine_learning.predictive_analysis import (
-                                                    load_model_and_predict,
-                                                    resize_input_image,
-                                                    plot_predictions_probabilities
-                                                    )
 
 
 def ModelPredictionAnalysis():
     st.write("Testing the 'model_prediction_analysis' page and it seems to work fine!")
-
-
